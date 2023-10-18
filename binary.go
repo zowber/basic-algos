553091bcbdd17ddb5d6421920d1ed3ec43e7822b
zowber <andy@zowber.com>
2023-10-18 22:42:33 +0100

    comments

--- binary.go
+++ binary.go
@@ -4,6 +4,8 @@ import (
 	"math"
 )
 
+// checks if an array contains the target
+
 func Binary_Search(arr []int, target int) bool {
 
 	low := 0
@@ -20,8 +22,12 @@ func Binary_Search(arr []int, target int) bool {
 		if val == target {
 			return true
 		} else if val > target {
+			// the target is lower than mid point
+			// make the mid point the new high bound
 			high = mid
 		} else if val < target {
+			// the target is higher than the mid point
+			// make mid + 1 the new low bound
 			low = mid + 1
 		}
 	}
